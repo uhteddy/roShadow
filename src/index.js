332f8d079c77f09550ec675aadf973492bc89a02
Teddy Hartling <httpdevz@gmail.com>
2022-09-12 13:59:47 -0400

    Added more things lol.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 const { app, BrowserWindow } = require('electron')
+const { setupTitlebar, attachTitlebarToWindow } = require("custom-electron-titlebar/main");
+
+setupTitlebar();
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 425,
+        height: 450,
+        maximizable: false,
+        minimizable: false,
+        title: "roShadow",
+        autoHideMenuBar: true,
+        titleBarStyle: 'hidden',
+        webPreferences: {
+            devTools: false,
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js'),
+        }
     })
 
     win.loadFile('./index.html')
+    attachTitlebarToWindow(win);
 }
 
 app.whenReady().then(() => {
