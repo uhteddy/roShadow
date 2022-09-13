3ef1fa307d29626e82e5947bada260bad170caba
uhteddy <54421295+uhteddy@users.noreply.github.com>
2022-09-12 21:48:25 -0400

    Testing

--- src/index.js
+++ src/index.js
@@ -1,28 +1,53 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 const { setupTitlebar, attachTitlebarToWindow } = require("custom-electron-titlebar/main");
 
-setupTitlebar();
+require('update-electron-app')()
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 425,
-        height: 450,
-        maximizable: false,
-        minimizable: false,
-        title: "roShadow",
-        autoHideMenuBar: true,
-        titleBarStyle: 'hidden',
-        webPreferences: {
-            devTools: false,
-            nodeIntegration: true,
-            preload: path.join(__dirname, 'preload.js'),
-        }
-    })
-
-    win.loadFile('./index.html')
-    attachTitlebarToWindow(win);
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+// eslint-disable-next-line global-require
+if (require('electron-squirrel-startup')) {
+  app.quit();
 }
 
-app.whenReady().then(() => {
-    createWindow()
-})
+setupTitlebar();
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: "/images/roShadow.png",
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  attachTitlebarToWindow(mainWindow);
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
--- src/preload.js
+++ src/preload.js
@@ -1,4 +1,6 @@
-import { Titlebar } from "custom-electron-titlebar";
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { Titlebar } = require("custom-electron-titlebar");
 
 window.addEventListener('DOMContentLoaded', () => {
   // Title bar implemenation
